481b70e3011056d8ff4d6c74eb85eb07108141f0
S.B <65360079+fish-hue@users.noreply.github.com>
2023-12-01 00:42:21 -0600

    Update thw.sh

--- thw.sh
+++ thw.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
-# Clear the history file completely
-cat /dev/null > ~/.bash_history
+read -p "Would you like to clear your terminal history? y/N?" choice
 
-# Clear the history entries in memory
-history -c
+if [[ "$choice" =~ ^[Yy]$ ]]; then
+  echo "Clearing terminal history..."
+  history -c && history -w
+elif [[ "$choice" =~ ^[Nn]$ ]]; then
+  echo "Exiting..."
+  exit
+else
+  echo "Invalid choice. Exiting..."
+  exit 1
+fi
 
 # Print a success message
 echo "Terminal history has been cleared successfully."
+
+# Clear Sudo History
+read -p "Would you like to clear the sudo history? y/N?" sudo_choice
+
+if [[ "$sudo_choice" =~ ^[Yy]$ ]]; then
+  echo "Clearing sudo history..."
+  sudo su -c 'cat /dev/null > ~/.bash_history'  # Clear root's history file
+  sudo bash -c 'history -c && history -w'       # Clear current user's sudo history
+elif [[ "$sudo_choice" =~ ^[Nn]$ ]]; then
+  echo "Skipping sudo history clearing..."
+else
+  echo "Invalid choice. Skipping sudo history clearing..."
+fi
 
 # Exit the current shell
 exit
